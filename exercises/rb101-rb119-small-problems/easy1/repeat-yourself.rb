4c506876eb3304d6b2d796d66735fb1479f93664
TacoMilitary <wlolams@outlook.com>
2026-02-09 20:15:22 -0600

    (Repeat Yourself) Fixed for positive integer requirement.

--- exercises/rb101-rb119-small-problems/easy1/repeat-yourself.rb
+++ exercises/rb101-rb119-small-problems/easy1/repeat-yourself.rb
@@ -1,4 +1,5 @@
 def repeat(repeated_string="", repeat_times=1)
+    repeat_times = 1 if repeat_times <= 0
     repeat_times.times {puts repeated_string}
     # another way of doing it
     #(puts repeated_string + "\n#{repeated_string}" * (repeat_times - 1)) if repeat_times > 0
