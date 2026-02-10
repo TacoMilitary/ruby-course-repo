6393720df8899ce5fd232a25f84c3359cf83b664
TacoMilitary <wlolams@outlook.com>
2026-02-09 20:19:12 -0600

    (Repeat Yourself) rubocop

--- exercises/rb101-rb119-small-problems/easy1/repeat-yourself.rb
+++ exercises/rb101-rb119-small-problems/easy1/repeat-yourself.rb
@@ -1,8 +1,8 @@
-def repeat(repeated_string="", repeat_times=1)
-    repeat_times = 1 if repeat_times <= 0
-    repeat_times.times {puts repeated_string}
-    # another way of doing it
-    #(puts repeated_string + "\n#{repeated_string}" * (repeat_times - 1)) if repeat_times > 0
+def repeat(repeated_string = '', repeat_times = 1)
+  repeat_times = 1 if repeat_times <= 0
+  repeat_times.times { puts repeated_string }
+  # another way of doing it
+  # (puts repeated_string + "\n#{repeated_string}" * (repeat_times - 1)) if repeat_times > 0
 end
 
 repeat('Hello', 3)
