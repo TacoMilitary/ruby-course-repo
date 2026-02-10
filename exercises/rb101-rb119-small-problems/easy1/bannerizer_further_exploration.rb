a314f2aeb31539e5ccd5cb2b71a2268293e31743
TacoMilitary <wlolams@outlook.com>
2026-02-10 17:23:36 -0600

    Ruby > RB101-RB119 - Small Problems > Easy 1 > Bannerizer Further Exploration (i lied)

--- exercises/rb101-rb119-small-problems/easy1/bannerizer_further_exploration.rb
+++ exercises/rb101-rb119-small-problems/easy1/bannerizer_further_exploration.rb
@@ -1,9 +1,19 @@
 # frozen_string_literal: true
 MAX_TEXT_WIDTH = 76
 
-def format_multiline_text(original_text = '')
-  last_wrapped_index = 0
+def format_multiline_text(original_text = '', truncate = false)
   original_length = original_text.length
+  
+  if truncate
+    final_line_text = "| #{original_text} |"
+    max_text_length = MAX_TEXT_WIDTH - 4 
+    if original_length >= max_text_length # 3 for the periods + 1 for array index
+      final_line_text = "| #{original_text[0..max_text_length]}... |" 
+    end
+    return final_line_text 
+  end
+
+  last_wrapped_index = 0
   final_char_index = original_length - 1
   text_array = []
   until last_wrapped_index >= final_char_index
@@ -23,8 +33,8 @@ def format_multiline_text(original_text = '')
   text_array.join("\n")
 end
 
-def print_in_box(user_text)
-  formatted_user_text = (user_text == '' || user_text.nil?) ? '|  |' : format_multiline_text(user_text) # "| #{user_text} |"
+def print_in_box(user_text, truncate = false)
+  formatted_user_text = (user_text == '' || user_text.nil?) ? '|  |' : format_multiline_text(user_text, truncate) # "| #{user_text} |"
 
   text_max_length = user_text.length.clamp(0, MAX_TEXT_WIDTH)
 
@@ -35,8 +45,8 @@ def print_in_box(user_text)
   puts final_text_box
 end
 
-print_in_box('To boldy go where no one has gone before.')
+print_in_box('To boldy go where no one has gone before.', true)
 print_in_box('Bright ideas travel far when curiosity, patience, and courage.')
-print_in_box('Bright ideas travel far when curiosity, patience, and courage walk together now.')
-print_in_box('Bright ideas travel far when curiosity, patience, and courage walk together now. Chocolate cupcakes and strawberry pies.')
+print_in_box('Bright ideas travel far when curiosity, patience, and courage walk together now.', false)
+print_in_box('Bright ideas travel far when curiosity, patience, and courage walk together now. Chocolate cupcakes and strawberry pies.', true)
 print_in_box('')
