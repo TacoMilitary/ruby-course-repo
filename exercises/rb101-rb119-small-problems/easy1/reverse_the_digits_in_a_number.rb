36bf50c447b42c9a6dd291d4f182b0f60a730a65
TacoMilitary <wlolams@outlook.com>
2026-02-11 17:28:04 -0600

    Ruby > RB101-RB119 - Small Problems > Easy 1 > Reverse the Digits in a Number (final) + rubocop

--- exercises/rb101-rb119-small-problems/easy1/reverse_the_digits_in_a_number.rb
+++ exercises/rb101-rb119-small-problems/easy1/reverse_the_digits_in_a_number.rb
@@ -1,13 +1,15 @@
 # frozen_string_literal: true
 
 def reversed_number(n = 1)
-  n = n.positive? ? n.to_s : "1"
-  n.reverse.to_i
+  n = n.positive? ? n.to_s : '1'
+  num_type = n.is_a?(Integer) ? :to_i : :to_f
+  n.reverse.public_send(num_type)
 end
 
-puts reversed_number(12345) == 54321
-puts reversed_number(12213) == 31221
+puts reversed_number(12_345) == 54_321
+puts reversed_number(12_213) == 31_221
 puts reversed_number(456) == 654
-puts reversed_number(12000) == 21
-puts reversed_number(12003) == 30021
+puts reversed_number(12_000) == 21
+puts reversed_number(12_003) == 30_021
 puts reversed_number(1) == 1
+puts reversed_number(12.0037)
