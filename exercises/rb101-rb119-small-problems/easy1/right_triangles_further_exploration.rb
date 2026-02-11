955fbaf57987fe5c0f7741fb147385cdca439de1
TacoMilitary <wlolams@outlook.com>
2026-02-11 15:04:16 -0600

    Ruby > RB101-RB119 - Small Problems > Easy 1 > Right Triangles Further Exploration (refactoring)

--- exercises/rb101-rb119-small-problems/easy1/right_triangles_further_exploration.rb
+++ exercises/rb101-rb119-small-problems/easy1/right_triangles_further_exploration.rb
@@ -1,28 +1,17 @@
 def triangle(n = 1, face_left: true, upright: true)
   n = 1 if n <= 0
 
-  
   triangles_array =
-  if face_left
-    Array.new(n) do |i|
-      if upright
-        ('*' * (i + 1)).rjust(n)
-      else
-        ('*' * (n - i)).rjust(n)
-      end
-    end
-  else
-    Array.new(n) do |i| 
-      if upright
-        ('*' * (i + 1)).ljust(n)
-      else
-        ('*' * (n - i)).ljust(n)
-      end
-    end
+	Array.new(n) do |i|
+		alignment_formula = upright ? (i + 1) : (n - i)
+    '*' * alignment_formula
   end
+  
+	triangles_array.map! {|asterisks| asterisks.rjust(n)} if face_left
 
   puts triangles_array.join("\n")
 end
 
 triangle(5, upright: false)
+triangle(3)
 triangle(9, face_left: false, upright: false)
