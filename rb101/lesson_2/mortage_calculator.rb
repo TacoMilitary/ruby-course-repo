575203049a2a77759e124eec711aee60407f535e
TacoMilitary <wlolams@outlook.com>
2026-02-16 21:40:43 -0600

    Ruby > RB101 Programming Foundations with Ruby: Basics > Lesson 2 > Mortage Calculator

---
+++ rb101/lesson_2/mortage_calculator.rb
@@ -0,0 +1,57 @@
+# frozen_string_literal: true
+
+LOAN_PRINCIPAL_PROMPT = 'How much is your Loan Principal?'
+LOAN_TERM_PROMPT = 'How many months is your Loan Term?'
+INTEREST_APR_PROMPT = <<-HEREDOC
+Will we be using an Interest Rate or an APR?
+ (interest)
+ (apr)?
+HEREDOC
+
+APR_PROMPT = 'What is the your APR?'
+INTEREST_PROMPT = 'What is your Interest Rate?'
+
+DEFAULT_RATE_RESPONSE = "Okay, so we're not including any Rates."
+RATE_RESPONSES = {
+  'interest' => "So we're working with Interest Rate.",
+  'apr' => "So we're using the Annual Percentage Rate."
+}
+
+def prompt_user(prompt)
+  puts prompt
+  print '> '
+  gets.chomp
+end
+
+def format_number(n)
+  n > n.to_i ? n.truncate(2) : n.to_i
+end
+
+def loan_calculator
+  puts 'Hello! Welcome the mortage calculator!'
+  loan_p = prompt_user(LOAN_PRINCIPAL_PROMPT).to_f
+  loan_months = prompt_user(LOAN_TERM_PROMPT).to_f
+
+  rate_response = prompt_user(INTEREST_APR_PROMPT).downcase.strip
+  rate_confirmation_reply = RATE_RESPONSES[rate_response] || DEFAULT_RATE_RESPONSE
+  puts "#{rate_confirmation_reply}\n"
+  
+  monthly_rate = 0.0
+  final_monthly = 0.0
+  if rate_confirmation_reply != DEFAULT_RATE_RESPONSE
+    if rate_response == 'apr'
+      monthly_rate = prompt_user(APR_PROMPT).to_f / 12.0
+    else
+      monthly_rate = prompt_user(INTEREST_PROMPT).to_f
+    end
+    monthly_rate /= 100
+    final_monthly = loan_p * ( monthly_rate / (1 - (1 + monthly_rate)**(-loan_months)) )
+  else
+    final_monthly = loan_p / loan_months
+  end
+  
+  number_to_display = format_number(final_monthly)
+  puts "Your monthly payment comes out to $#{number_to_display}."
+end
+
+loan_calculator
