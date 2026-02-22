afa60aab56fc41ff33b51e6706ebde82cb5b15c9
TacoMilitary <wlolams@outlook.com>
2026-02-22 00:22:58 -0600

    Ruby > RB101 Programming Foundations with Ruby: Basics > Lesson 2 > Rock Paper Scissors (Remake step 2)

--- rb101/lesson_2/rmk_rock_paper_scissors.rb
+++ rb101/lesson_2/rmk_rock_paper_scissors.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
+require 'yaml'
+
+GAME_TXT = YAML.load_file('rps_remake_txt.yml')
 
 CHOICE_WIN_RULES= {
   'rock' => 'scissors',
@@ -14,8 +17,59 @@ def prompt_user(prompt)
   gets.chomp.strip
 end
 
+def clear_screen
+  unless system('clear')
+    puts "\n-----------"
+  end
+end
+
+def divide_screen
+  puts "\n-----------"
+end
+
+def display_error(message = 'Unknown error!')
+  puts "[ERROR]: #{message}"
+end
+
+def user_rps_choice
+  loop do
+    user_input = prompt_user(GAME_TXT['before_shoot']).downcase
+
+    if POSSIBLE_CHOICES.include?(user_input)
+      return user_input
+    else
+      display_error(GAME_TXT['invalid_choice'])
+      divide_screen
+    end
+  end
+end
+
+def win_against?(choice_1, choice_2)
+  choice_1_wins_against = CHOICE_WIN_RULES[choice_1]
+  
+  choice_1_wins_against == choice_2 ? true : false
+end
+
+def cpu_rps_choice
+  POSSIBLE_CHOICES.sample
+end
+
 def game_start
-  puts ''
+  clear_screen
+
+  user_choice = user_rps_choice
+
+  puts "\n#{GAME_TXT['after_shoot']}"
+
+  computer_choice = cpu_rps_choice
+
+  if win_against?(user_choice, computer_choice)
+    puts 'WIN!'
+  elsif win_against?(computer_choice, user_choice)
+    puts 'LOSS!'
+  else
+    puts 'TIE!'
+  end
 end
 
 game_start
