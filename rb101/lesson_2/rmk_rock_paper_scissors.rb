027cf580e871aac46e67b991f43513651e99eb68
TacoMilitary <wlolams@outlook.com>
2026-02-22 00:44:59 -0600

    Ruby > RB101 Programming Foundations with Ruby: Basics > Lesson 2 > Rock Paper Scissors (Remake step 3)

--- rb101/lesson_2/rmk_rock_paper_scissors.rb
+++ rb101/lesson_2/rmk_rock_paper_scissors.rb
@@ -31,7 +31,7 @@ def display_error(message = 'Unknown error!')
   puts "[ERROR]: #{message}"
 end
 
-def user_rps_choice
+def player_rps_get
   loop do
     user_input = prompt_user(GAME_TXT['before_shoot']).downcase
 
@@ -50,25 +50,62 @@ def win_against?(choice_1, choice_2)
   choice_1_wins_against == choice_2 ? true : false
 end
 
-def cpu_rps_choice
+def cpu_rps_get
   POSSIBLE_CHOICES.sample
 end
 
-def game_start
-  clear_screen
-
-  user_choice = user_rps_choice
-
-  puts "\n#{GAME_TXT['after_shoot']}"
-
-  computer_choice = cpu_rps_choice
-
-  if win_against?(user_choice, computer_choice)
-    puts 'WIN!'
-  elsif win_against?(computer_choice, user_choice)
-    puts 'LOSS!'
+def match_outcome_get(player_1_rps, player_2_rps)
+  if win_against?(player_1_rps, player_2_rps)
+    'win'
+  elsif win_against?(player_2_rps, player_1_rps)
+    'lose'
   else
-    puts 'TIE!'
+    'draw'
+  end
+end
+
+def display_results(outcome)
+  case outcome
+  when 'win' then puts GAME_TXT['win_message']
+  when 'lose' then puts GAME_TXT['lose_message']
+  else puts GAME_TXT['draw_message']
+  end
+end
+
+def ask_for_rematch?
+  loop do
+    user_input = prompt_user(GAME_TXT['rematch_ask']).downcase
+
+    case user_input
+    when 'y'
+      return true
+    when 'n'
+      return false
+    else
+      display_error(GAME_TXT['invalid_choice'])
+      divide_screen
+    end
+  end
+end
+
+def game_start
+  continue_game = true
+
+  clear_screen
+  while continue_game
+    player_choice = player_rps_get
+
+    puts "\n#{GAME_TXT['after_shoot']}"
+    
+    computer_choice = cpu_rps_get
+
+    player_outcome = match_outcome_get(player_choice, computer_choice)
+
+    display_results
+
+    unless player_outcome == 'tie'
+      continue_game = ask_for_rematch?
+    end
   end
 end
 
